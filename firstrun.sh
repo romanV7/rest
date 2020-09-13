dd09500d570b0c3c91f72831b20fbb195716d320
Roman <mister02.rv@gmail.com>
2020-09-13 19:59:09 +0300

    add bash

---
+++ firstrun.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "Start running..."
+
+docker run -d \
+  --restart unless-stopped \
+  --name some-mysql \
+  -v $PWD/mysql-data:/var/lib/mysql \
+  -e MYSQL_ROOT_PASSWORD=root \
+  -e MYSQL_DATABASE=test_db \
+  -p 3306:3306 mysql:5.7.27
+
+echo "Finish"
--- src/config.js
+++ src/config.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
+
 module.exports = {
-  host: "127.0.0.1",
-  user: "root",
-  database: "test_db",
-  password: "root",
+  host: process.env.HOST || "127.0.0.1",
+  user: process.env.DATABASE_USER || "root",
+  database: process.env.DATABASE_DB || "test_db",
+  password: process.env.DATABASE_PASSWORD || "root",
   insecureAuth : true
 };
